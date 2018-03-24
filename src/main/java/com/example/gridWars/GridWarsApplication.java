16ab0776bda4927fcad45fa3a6920da03a9f54b3
Prakash Iyer <prakash_iyer@arvindinternet.com>
2018-03-24 18:44:44 +0530

    initialized

---
+++ src/main/java/com/example/gridWars/GridWarsApplication.java
@@ -0,0 +1,12 @@
+package com.example.gridWars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GridWarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GridWarsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/gridWars/GridWarsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.gridWars;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GridWarsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
